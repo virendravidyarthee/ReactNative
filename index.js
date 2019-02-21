74b3da106fb937f3ab35738b03177f0287945864
Virendra <virendravidyarthee@gmail.com>
2019-02-21 16:29:35 +0530

    Implementing react-native-navigation

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-/**
- * @format
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
+import { Navigation } from "react-native-navigation";
+import App from './src/App';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+Navigation.registerComponent(`navigation.assignment.app`, () => App);
+
+Navigation.events().registerAppLaunchedListener(() => {
+    Navigation.setRoot({
+        root: {
+            stack: {
+                children:[{
+                    component:{
+                        name: "navigation.assignment.app",
+                        options:{
+                            topBar:{
+                                title:{
+                                    text: "App"
+                                }
+                            }
+                        }
+                    }
+                }]
+            }
+        }
+    });
+});
