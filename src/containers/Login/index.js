1d2f5179069a939843f4975ed84b8b4059e83468
Virendra <virendravidyarthee@gmail.com>
2019-02-22 18:35:41 +0530

    Implementing keyboard avoiding scroll view

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import {StyleSheet, TextInput, View, Button, ActivityIndicator, Text} from 'react-native';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view'
 import {connect} from 'react-redux'
 import TextInputLayout from "../../components/TextInputLayout";
 import {getEmail, isLoading, getPassword} from "./reducer";
@@ -8,40 +9,42 @@ import {setEmail, setPassword, logUserIn} from "./actions";
 class Login extends Component<Props> {
 
     render() {
-        const {dispatch, email, password, isLoading} = this.props
+        const {dispatch, email, password, isLoading} = this.props;
         return (
-            <View style={styles.container}>
-                <TextInputLayout
-                    style={styles.textInputLayout}>
-                    <TextInput
-                        style={styles.input}
-                        placeholder={'Email'}
-                        onChangeText={value => {
-                            dispatch(setEmail(value.trim()))
-                        }}/>
-                </TextInputLayout>
-                <TextInputLayout
-                    style={styles.textInputLayout}>
-                    <TextInput
-                        style={styles.input}
-                        secureTextEntry={true}
-                        placeholder={'Password'}
-                        onChangeText={value => {
-                            dispatch(setPassword(value.trim()))
-                        }}/>
-                </TextInputLayout>
-                <View style={styles.buttonContainer}>
-                    <Button
-                        title='Login'
-                        onPress={() => {
-                            dispatch(logUserIn(email, password))
-                        }}
-                        disabled={!email || !password}/>
+            <KeyboardAwareScrollView contentContainerStyle={styles.container}>
+                <View style={styles.formContainer}>
+                    <TextInputLayout
+                        style={styles.textInputLayout}>
+                        <TextInput
+                            style={styles.input}
+                            placeholder={'Email'}
+                            onChangeText={value => {
+                                dispatch(setEmail(value.trim()))
+                            }}/>
+                    </TextInputLayout>
+                    <TextInputLayout
+                        style={styles.textInputLayout}>
+                        <TextInput
+                            style={styles.input}
+                            secureTextEntry={true}
+                            placeholder={'Password'}
+                            onChangeText={value => {
+                                dispatch(setPassword(value.trim()))
+                            }}/>
+                    </TextInputLayout>
+                    <View style={styles.buttonContainer}>
+                        <Button
+                            title='Login'
+                            onPress={() => {
+                                dispatch(logUserIn(email, password))
+                            }}
+                            disabled={!email || !password}/>
+                    </View>
+                    <ActivityIndicator
+                        style={{opacity: isLoading ? 1 : 0}}  //https://github.com/facebook/react-native/issues/9023
+                        animating={true}/>
                 </View>
-                <ActivityIndicator
-                    style={{opacity: isLoading ? 1 : 0}}  //https://github.com/facebook/react-native/issues/9023
-                    animating={true}/>
-            </View>
+            </KeyboardAwareScrollView>
         )
     }
 }
@@ -49,9 +52,11 @@ class Login extends Component<Props> {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
         backgroundColor: '#FFFFFF',
+    },
+    formContainer: {
+        alignItems: 'center',
+        marginTop: 50
     },
     textInputLayout: {
         width: '85%'
